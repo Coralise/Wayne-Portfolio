a68870583400b63f9f1d7c540bdf1a6c8938034b
Wayne Gabule <waynegabule@gmail.com>
2026-02-04 19:36:12 +0800

    Update resume section to reflect professional experience and skills in full-stack development and multimedia production

--- app/resume/page.js
+++ app/resume/page.js
@@ -107,7 +107,7 @@ export default function Resume() {
                         <motion.p transition={defaultTransition}
                         variants={revealXOpacity}
                         className="indent-12 text-foreground-2nd mt-4 lg:w-1/2 text-justify leading-loose">
-                            I am a <span className="text-foreground font-semibold">BS in Information Technology</span> graduate from Ateneo de Davao University with freelance experience in <span className="text-foreground font-semibold">Java programming</span> and <span className="text-foreground font-semibold">video editing</span>. I strive to excel in these fields, including <span className="text-foreground font-semibold">motion graphics design, front-end development</span>, and many more.
+                            <span className="text-foreground font-semibold">BS in Information Technology</span> graduate from Ateneo de Davao University with professional experience in <span className="text-foreground font-semibold">full-stack development</span> and <span className="text-foreground font-semibold">motion graphics design</span>. Skilled in building scalable web solutions and creating engaging visual content, with a strong focus on <span className="text-foreground font-semibold">front-end development</span> and <span className="text-foreground font-semibold">multimedia/video production</span>.
                         </motion.p>
                     </motion.div>
 
