e3340dd7808986ac24fdcd59d686d216e3c3872c
Wayne Gabule <waynegabule@gmail.com>
2026-02-05 18:56:26 +0800

    Refactor header and contact sections for improved clarity and consistency

--- app/components/header.js
+++ app/components/header.js
@@ -13,14 +13,14 @@ export default function Header() {
     }
 
     return (
-        <header>
-            <div className="p-5 rounded-2xl hidden sm:flex gap-5 my-auto fixed container z-50">
+        <header className="">
+            <div className="p-4 hidden sm:flex gap-4 my-auto fixed z-50 w-full backdrop-blur-sm bg-white/5 border-b-[1px] border-white/10">
                 <Link href={'/'}><img alt="Logo" src="images/logo.png" className="size-5 hover:size-7 m-1 hover:m-0 transition-all duration-300 ease-out rotate-[-45deg] hover:rotate-[0deg] active:size-5 active:m-1" /></Link>
                 <Link href={'/'} className="hover:text-highlight transition-all ease-out duration-300 my-auto hover:-rotate-6 active:rotate-6 hover:font-semibold">Overview</Link>
                 <Link href={'/resume'} className="hover:text-highlight transition-all ease-out duration-300 my-auto hover:-rotate-6 active:rotate-6 hover:font-semibold">Resume</Link>
                 <Link href={'/programming-portfolio'} className="hover:text-highlight transition-all ease-out duration-300 my-auto hover:-rotate-6 active:rotate-6 hover:font-semibold">Programming</Link>
                 <Link href={'/motion-graphics-portfolio'} className="hover:text-highlight transition-all ease-out duration-300 my-auto hover:-rotate-6 active:rotate-6 hover:font-semibold">Motion Graphics</Link>
-                <Link href={'/contact'} className="hover:text-highlight transition-all ease-out duration-300 my-auto hover:-rotate-6 active:rotate-6 hover:font-semibold">Contact</Link>
+                <Link href={'/contact'} className="hover:text-highlight transition-all ease-out duration-300 my-auto hover:-rotate-6 active:rotate-6 hover:font-semibold">Contact Me</Link>
             </div>
             <div className={`flex w-screen z-40 justify-end absolute`}>
                 <button onClick={toggleNavBar} className={`${navBarOpened ? "max-sm:opacity-30 sm:opacity-0 sm:pointer-events-none" : "opacity-0 pointer-events-none"} transition-opacity duration-500 fixed w-screen h-screen bg-black -z-50`} />
@@ -30,7 +30,7 @@ export default function Header() {
                         <Link href={'/resume'} onClick={toggleNavBar}>Resume</Link>
                         <Link href={'/programming-portfolio'} onClick={toggleNavBar}>Programming</Link>
                         <Link href={'/motion-graphics-portfolio'} onClick={toggleNavBar}>Motion Graphics</Link>
-                        <Link href={'/contact'} onClick={toggleNavBar}>Contact</Link>
+                        <Link href={'/contact'} onClick={toggleNavBar}>Contact Me</Link>
                     </div>
                 </div>
             </div>
--- app/resume/page.js
+++ app/resume/page.js
@@ -399,10 +399,10 @@ function ContactMeCard({gradientHover, beforeMail, className, center=false}) {
                         <a href="https://github.com/Coralise" target="_blank" className="flex w-fit p-2 bg-neutral-400 rounded-lg transition-all duration-500 hover:bg-transparent text-background hover:text-[#6e5494] shadow-[transparent_0px_0px_5px_4px] hover:shadow-[#6e5494_0px_0px_5px_4px]">
                             <FaGithubAlt className="size-4" />
                         </a>
-                        {/* <a href="https://www.linkedin.com/in/sean-wayne-gabule-083a481a6/" target="_blank" className="flex w-fit p-2 bg-neutral-400 rounded-lg transition-all duration-500 hover:bg-transparent text-background hover:text-[#0077B5] shadow-[transparent_0px_0px_5px_4px] hover:shadow-[#0077B5_0px_0px_5px_4px]">
+                        <a href="https://www.linkedin.com/in/sean-wayne-gabule-083a481a6/" target="_blank" className="flex w-fit p-2 bg-neutral-400 rounded-lg transition-all duration-500 hover:bg-transparent text-background hover:text-[#0077B5] shadow-[transparent_0px_0px_5px_4px] hover:shadow-[#0077B5_0px_0px_5px_4px]">
                             <FaLinkedinIn className="size-4" />
                         </a>
-                        <a href="https://www.facebook.com/Waynezki" target="_blank" className="flex w-fit p-2 bg-neutral-400 rounded-lg transition-all duration-500 hover:bg-transparent text-background hover:text-[#1877F2] shadow-[transparent_0px_0px_5px_4px] hover:shadow-[#1877F2_0px_0px_5px_4px]">
+                        {/* <a href="https://www.facebook.com/Waynezki" target="_blank" className="flex w-fit p-2 bg-neutral-400 rounded-lg transition-all duration-500 hover:bg-transparent text-background hover:text-[#1877F2] shadow-[transparent_0px_0px_5px_4px] hover:shadow-[#1877F2_0px_0px_5px_4px]">
                             <FaFacebookF className="size-4" />
                         </a> */}
                     </div>
